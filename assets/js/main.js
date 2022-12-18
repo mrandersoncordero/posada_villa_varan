15606f75c61060bbdba8869ba7419ff36b90cf6c
mrandersoncordero <acorderofigueroa7@gmail.com>
2022-12-18 16:20:41 -0430

    Todo lo que llevo por los momentos

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.products');
+ScrollReveal().reveal('.article', {delay: 500});
+ScrollReveal().reveal('.cards-banner-one', {delay: 500});
+ScrollReveal().reveal('.cards-banner-two', {delay: 500});
